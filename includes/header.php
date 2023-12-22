b3dbb227cf29669cdd4bde52d663b4abf9552b19
Smiath <smiath.yt@gmail.com>
2023-12-22 19:43:47 +0100

    [FIX] : The link in the "header.php" was'nt good 	- old : "event-liste.php" 	- new : "event-list.php"

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
             <!-- Liens pour les utilisateurs connectés -->
             <a href="profil.php">Profil</a>
             <a href="my-events.php">Mes Événements</a>
-            <a href="event-liste.php">Liste des Événements</a>
+            <a href="event-list.php">Liste des Événements</a>
             <a href="creation-event.php">Créer un Événement</a>
             <a href="logout.php">Déconnexion</a>
         <?php else : ?>
